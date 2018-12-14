b53e7d94a0b5ca03967c623bcca9fa27fdfa40b2
elouanKeryell-Even <elouan.keryell@gmail.com>
2018-12-14 15:04:41 +0100

    Implement modular initialization

--- arlas-exploration-stack-initializer/base.bash
+++ arlas-exploration-stack-initializer/base.bash
@@ -1,16 +1,37 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [[ "$debug" == true ]]; then
+  set -x
+fi
+
+################################################################################
+# Variables
+################################################################################
+
+declare -A initialization_steps_mapping
+initialization_steps_mapping['es-index']=create_elasticsearch_index
+initialization_steps_mapping['index-data']=index_data
+initialization_steps_mapping['server-collection']=create_server_collection
+initialization_steps_mapping['ui-conf']=load_WUI_configuration
+initialization_steps_mapping['ui-map-conf']=load_WUI_map_configuration
+
 
 ################################################################################
 # Functions
 ################################################################################
 
 create_elasticsearch_index () {
+  wait_for_elasticsearch
+
   get_index_response_http_code=$(curl -s -o /dev/null -w "%{http_code}" $elasticsearch_options "$elasticsearch/$elasticsearch_index")
   if (( $get_index_response_http_code == 200 )); then
     log "Deleting pre-existing index from elasticsearch..."
     curl -s $elasticsearch_options -X DELETE "$elasticsearch/$elasticsearch_index"
     echo
   elif (( $get_index_response_http_code == 404 )); then
+    log "Done."
     :
   else
     log_error "Undefined response HTTP code while verifying if index is already existing, exiting."
@@ -23,35 +44,45 @@ create_elasticsearch_index () {
   if (( $elasticsearch_put_index_return_code != 200 )); then
     exit 1
   fi
+  log "Done."
 }
 
 create_server_collection (){
   log "Force elasticsearch index in ARLAS server collection..."
   jq -r ".index_name=\"$elasticsearch_index\"" /initialization/server/collection.json > /tmp/server_collection.json
   mv /tmp/server_collection.json /initialization/server/collection.json
+  log "Done."
 
   log_without_blank_line "Waiting for ARLAS-server to be ready..."
   wait_for "(( \$(curl -s -o /dev/null -w \"%{http_code}\" $server_URL_for_initializer/admin/healthcheck) == 200 ))"
+  log "Ready."
 
   log "Creating collection in ARLAS server..."
   curl -s -X PUT --header 'Content-Type: application/json;charset=utf-8' --header 'Accept: application/json' -d@/initialization/server/collection.json "$server_URL_for_initializer/arlas/collections/$server_collection_name"
   echo
+  log "Done."
 }
 
-index_data (){
+index_data () {
   log "Indexing data in elasticsearch..."
   if [[ -f /initialization/data_ingestion/data ]]; then
     cat /initialization/data_ingestion/data | "./logstash-$logstash_version/bin/logstash" -f "/initialization/data_ingestion/logstash_configuration"
+    log "Done."
   else
     "./logstash-$logstash_version/bin/logstash" -f "/initialization/data_ingestion/logstash_configuration"
   fi
-  
 }
 
 load_WUI_configuration () {
   log "Loading WUI configuration..."
   jq -r ".arlas.server.url=\"$server_URL_for_client/arlas\" | .arlas.server.collection.name=\"$server_collection_name\"" "/initialization/WUI/config.json" > /wui-configuration/config.json
+  log "Done."
+}
+
+load_WUI_map_configuration () {
+  log "Loading WUI map configuration..."
   cp "/initialization/WUI/config.map.json" /wui-configuration/config.map.json
+  log "Done."
 }
 
 log () {
@@ -110,6 +141,7 @@ wait_for_elasticsearch () {
   # On single server configuration, the best status we can reach is yellow.
   # See https://www.elastic.co/guide/en/elasticsearch/reference/current/cluster-health.html
   wait_for "(( \$(curl -s -o /dev/null -w \"%{http_code}\" $elasticsearch_options $elasticsearch/_cluster/health?wait_for_status=yellow) == 200 ))"
+  log "Ready."
 }
 
 
@@ -136,6 +168,7 @@ variables=( \
   elasticsearch \
   elasticsearch_index \
   elasticsearch_user \
+  initialization_sequence \
   server_collection_name \
   server_URL_for_client \
   server_URL_for_initializer \
@@ -154,10 +187,32 @@ done
 
 echo "$break_line"
 
-wait_for_elasticsearch
-create_elasticsearch_index
-create_server_collection
-load_WUI_configuration
-index_data
+# Build initialization-steps array
+if [[ -v initialization_sequence ]]; then
+  IFS=',' read -r -a initialization_sequence_array <<< "$initialization_sequence"
+else
+  initialization_sequence_array=(
+      es-index
+      server-collection
+      ui-conf
+      ui-map-conf
+      index-data
+    )
+fi
 
-log_without_blank_line "Done"
+# Map initialization steps to functions
+functions_sequence=()
+for initialization_step in "${initialization_sequence_array[@]}"; do
+  if ! [[ -v "initialization_steps_mapping['$initialization_step']" ]]; then
+    log_error "Initialization step \`$initialization_step\` undefined, aborting."
+    exit 1
+  else
+    functions_sequence+=("${initialization_steps_mapping["$initialization_step"]}")
+  fi
+done
+
+for function in "${functions_sequence[@]}"; do
+  eval "$function"
+done
+
+log_without_blank_line "Initialization finished."
