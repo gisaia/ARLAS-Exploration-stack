cfe61ed61f00cda01f7e079c4b4e54f6d9c118dc
MohamedHamou <mohamed.hamou@gisaia.com>
2020-09-11 10:45:40 +0200

    Resolve MB discussions

--- add_data.sh
+++ add_data.sh
@@ -5,9 +5,10 @@ SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 PROJECT_ROOT_DIRECTORY="$SCRIPT_DIRECTORY"
 
 usage(){
-	echo "Usage: ./start.sh [--arlas-server-url] [--es-cluster] "
-	echo " -asu|--arlas-server-url       URL of a arlas-server service to use. If not set, $LOCAL_HOST:19999 is considered"
-	echo " -esc|--es-cluster             ES cluster to use. If not set, $LOCAL_HOST:9200/arlas is considered"
+	echo "Usage: ./start.sh [--arlas-server-url] [--es-cluster] [--es-credentials]"
+	echo " -asu|--arlas-server-url       URL of a arlas-server service to use. If not set, $LOCAL_HOST:19999/arlas is considered"
+	echo " -esc|--es-cluster             ES cluster to use. If not set, $LOCAL_HOST:9200 is considered"
+	echo " -escdr|--es-credentials       Credential to use to connect to ES Cluster (if necessary)"
     echo " -h|--help                     Display manual"
 	exit 1
 }
@@ -26,6 +27,10 @@ case $i in
     ;;
     -esc=*|--es-cluster=*)
     export ES_CLUSTER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -escdr=*|--es-credentials=*)
+    export ES_CREDENTIALS="${i#*=}"
     shift # past argument=value
     ;;
     *)
@@ -55,17 +60,17 @@ if [ ! -z ${ARLAS_SERVER_URL+x} ];
 fi 
 
 echo "## Deleting 'airport_index' in case it already exists"
-curl -f -X DELETE "${ES_CLUSTER}/airport_index?pretty"  || echo " !!! Couldn't reach Elasticsearch at ${ES_CLUSTER}"; echo ""; usage; exit -1;
+curl -f --user ${ES_CREDENTIALS} -X DELETE "${ES_CLUSTER}/airport_index?pretty"  || echo " !!! Couldn't reach Elasticsearch at ${ES_CLUSTER}"; echo ""; usage; exit -1;
 
 echo "## Creating 'airport_index'"
-curl -f -XPUT \
+curl -f --user ${ES_CREDENTIALS} -XPUT \
   -H 'Content-Type: application/json' \
   "${ES_CLUSTER}/airport_index?pretty" \
   --data @$SCRIPT_DIRECTORY/data/airport_mapping.json || echo " !!! Couldn't reach Elasticsearch at ${ES_CLUSTER}"; echo ""; usage; exit -1;
   
 echo "## Adding data to 'airport_index'"
 curl -s -H "Content-Type: application/json" \
-  -XPOST "${ES_CLUSTER}/airport_index/_bulk?pretty&refresh" \
+  --user ${ES_CREDENTIALS} -XPOST "${ES_CLUSTER}/airport_index/_bulk?pretty&refresh" \
   --data-binary @${SCRIPT_DIRECTORY}/data/data.txt > /dev/null
 echo "   -> Data added"
 
