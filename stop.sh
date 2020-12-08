f64fd9276e241306ebce48f59d9df3d88bca6a32
Matthieu Barbet @ Gisaia <matthieu.barbet@gisaia.com>
2020-12-08 13:52:37 +0100

    Fix stop.sh script

--- stop.sh
+++ stop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -o errexit -o pipefail
-export $(eval "echo \"$(cat .env)\"" | xargs)
+#Set env variables using env.sh
+source ./env.sh;
+#Set variables in .env as env variable id .env file exist
+envFile=.env
+if [ -f "$envFile" ];then
+    export $(eval "echo \"$(cat .env)\"" | xargs)
+fi
 eval "docker-compose down"
