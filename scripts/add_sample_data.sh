59777281e7acd4b2428a6d3a86180f9096fb6020
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-08-28 09:58:23 +0200

    add permission url

--- scripts/add_sample_data.sh
+++ scripts/add_sample_data.sh
@@ -18,15 +18,13 @@ if [ ${CONF} == "local.iam.admin" ]; then
     echo "ask to reset password"
     curl -kX POST https://localhost/arlas_iam_server/users/resetpassword -H 'Content-Type: application/json;charset=utf-8' -d "user@org.com"
     echo "fetch token"
-    TOKEN=`docker logs arlas-iam-server --tail 100 | grep "Reset token" | tail-1 | awk -F 'token: ' '{print $2}'`
+    TOKEN=`docker logs arlas-iam-server --tail 100 | grep "Reset token" | tail -1 | awk -F 'token: ' '{print $2}'`
     echo "set password to 'secret'"
     curl -kX POST https://localhost/arlas_iam_server/users/${USERID}/reset/${TOKEN} -H 'Content-Type: application/json;charset=utf-8' -d "secret"
 fi
 
-yes|arlas_cli --config-file /tmp/arlas-cli.yaml collections --config ${CONF} delete courses
-
 curl https://raw.githubusercontent.com/gisaia/arlas_cli/master/tests/sample.json -o sample/sample.json
 arlas_cli --config-file /tmp/arlas-cli.yaml indices --config ${CONF} mapping sample/sample.json --nb-lines 200 --field-mapping track.timestamps.center:date-epoch_second --field-mapping track.timestamps.start:date-epoch_second --field-mapping track.timestamps.end:date-epoch_second --no-fulltext cargo_type --push-on courses
 arlas_cli --config-file /tmp/arlas-cli.yaml indices --config ${CONF} data courses sample/sample.json
-arlas_cli --config-file /tmp/arlas-cli.yaml collections --config ${CONF} create courses --index courses --display-name courses --id-path track.id --centroid-path track.location --geometry-path track.trail --date-path track.timestamps.center --no-public --owner org.com
+arlas_cli --config-file /tmp/arlas-cli.yaml collections --config ${CONF} create courses --index courses --display-name courses --id-path track.id --centroid-path track.location --geometry-path track.trail --date-path track.timestamps.center --public --owner org.com
 arlas_cli --config-file /tmp/arlas-cli.yaml persist --config ${CONF} add sample/dashboard.json config.json --name "Course Dashboard" --reader "group/config.json/org.com"
