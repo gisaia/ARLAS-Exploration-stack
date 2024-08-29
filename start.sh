fbff4c4c47598903c15d902dd8a985703eaae20d
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-08-29 17:58:41 +0200

    debug test

--- start.sh
+++ start.sh
@@ -33,5 +33,7 @@ fi
 #    set +e
 #    docker compose -p arlas-exploration-stack $ENV_FILES -f dc/ref-dc-elastic-init.yaml -f dc/ref-dc-elastic.yaml -f dc/ref-dc-net.yaml up -d --wait --wait-timeout 300
 #    set -e
-
+set +e
 docker compose -p arlas-exploration-stack $ENV_FILES $COMPOSE_FILES up -d --remove-orphans --wait --wait-timeout 300 $COMPOSE_SERVICES
+set -e
+docker logs elasticsearch
