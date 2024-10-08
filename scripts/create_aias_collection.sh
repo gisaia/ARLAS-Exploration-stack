62421496dcda6c6398c09b3edefdb4749ce08c0a
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-10-08 16:17:56 +0200

    add aias configurations and start

---
+++ scripts/create_aias_collection.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit -o pipefail
+. conf/aias.env
+ORG=org.com
+[ -z "$1" ] && echo "Please provide the ARLAS configuration name" && exit 1;
+[ -z "$2" ] && echo "Please provide the collection name" && exit 1;
+[ -z "$3" ] && echo "Please provide the index name" && exit 2;
+[ ! -z "$4" ] && ORG=$4 ;
+
+CONF=$1
+COLLECTION=$2
+INDEX=$3
+
+GROUPS_PARAMS='--reader group/config.json/org.com --writer group/config.json/org.com'
+USER_CONF="local.iam.user"
+
+echo "Create collection '${COLLECTION}' on index '${INDEX}'"
+arlas_cli --config-file /tmp/arlas-cli.yaml \
+    collections --config ${CONF} create ${COLLECTION} \
+    --index ${INDEX} --display-name ${COLLECTION} \
+    --id-path id \
+    --centroid-path centroid \
+    --geometry-path geometry \
+    --date-path properties.datetime \
+    --no-public \
+    --owner ${ORG} \
+    --orgs ${ORG}
--- scripts/reset_data.sh
+++ scripts/reset_data.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-docker volume rm arlas-test-data-es arlas-test-persist arlas-test-postgres
+docker volume rm arlas-test-data-es arlas-test-persist arlas-test-postgres arlas-test-data-minio
