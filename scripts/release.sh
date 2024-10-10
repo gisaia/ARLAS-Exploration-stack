881fb8d7b18b89ceb5027fdad1884ab2743766b1
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2024-10-10 14:52:42 +0200

    add generated doc for aias

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,13 +12,13 @@ python3.10 scripts/generate_dc_doc.py \
     dc/ref-dc-arlas-wui.yaml \
     dc/ref-dc-protomaps.yaml \
     dc/ref-dc-apisix.yaml \
-    conf/versions.env\
-    conf/elastic.env\
-    conf/arlas.env\
-    conf/persistence-file.env\
-    conf/permissions.env\
-    conf/apisix.env\
-    conf/restart_strategy.env\
+    conf/versions.env \
+    conf/elastic.env \
+    conf/arlas.env \
+    conf/persistence-file.env \
+    conf/permissions.env \
+    conf/apisix.env \
+    conf/restart_strategy.env \
     conf/stack.env \
     > docker_compose_services_simple.md
 
@@ -35,13 +35,13 @@ python3.10 scripts/generate_dc_doc.py \
     dc/ref-dc-protomaps.yaml \
     dc/ref-dc-apisix.yaml \
     dc/ref-dc-postgres.yaml \
-    conf/versions.env\
-    conf/elastic.env\
-    conf/arlas.env\
-    conf/persistence-file.env\
-    conf/permissions.env\
-    conf/apisix.env\
-    conf/restart_strategy.env\
+    conf/versions.env \
+    conf/elastic.env \
+    conf/arlas.env \
+    conf/persistence-file.env \
+    conf/permissions.env \
+    conf/apisix.env \
+    conf/restart_strategy.env \
     conf/stack.env \
     conf/arlas_iam.env \
     conf/postgres.env \
@@ -60,19 +60,31 @@ python3.10 scripts/generate_dc_doc.py \
     dc/ref-dc-protomaps.yaml \
     dc/ref-dc-apisix.yaml \
     dc/ref-dc-postgres.yaml \
-    conf/versions.env\
-    conf/elastic.env\
-    conf/arlas.env\
-    conf/persistence-file.env\
-    conf/permissions.env\
-    conf/apisix.env\
-    conf/restart_strategy.env\
+    dc/ref-dc-aias-airs.yaml \
+    dc/ref-dc-aias-aproc-proc.yaml \
+    dc/ref-dc-aias-aproc-service.yaml \
+    dc/ref-dc-aias-fam-wui.yaml \
+    dc/ref-dc-aias-fam.yaml \
+    dc/ref-dc-aias-minio.yaml \
+    dc/ref-dc-aias-rabbitmq.yaml \
+    dc/ref-dc-aias-redis.yaml \
+    dc/ref-dc-aias-volumes.yaml \
+    dc/ref-dc-aias-agate.yaml \
+    conf/aias.env \
+    conf/minio.env \
+    conf/versions.env \
+    conf/elastic.env \
+    conf/arlas.env \
+    conf/persistence-file.env \
+    conf/permissions.env \
+    conf/apisix.env \
+    conf/restart_strategy.env \
     conf/stack.env \
     conf/arlas_iam.env \
     conf/postgres.env \
-    > docker_compose_services_iam.md
+    > docker_compose_services_aias.md
 
-git add docker_compose_services.md
-git commit -m "update docker_compose_services.md"
+git add docker_compose_services_*.md
+git commit -m "update docker compose services documentation"
 git tag -a ${VERSION} -m "ARLAS Exploration stack ${VERSION}"
 git push origin ${VERSION}
