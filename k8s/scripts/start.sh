82465f68607e3d432d6e1920b4076c1433b98133
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-07-24 20:02:53 +0200

    agate and fam

--- k8s/scripts/start.sh
+++ k8s/scripts/start.sh
@@ -6,6 +6,18 @@ if helm list -n default | grep -q '^aias'; then
   helm uninstall aias
 fi
 
+kubectl create configmap agate-files-configmap  \
+  --from-file=agate.yaml=conf/aias/agate.yaml  \
+  --from-file=roles.yaml=conf/aias/roles.yaml  \
+  --dry-run=client  \
+  -o yaml > k8s/charts/agate/templates/agate-files-configmap.yaml
+
+kubectl create configmap fam-files-configmap  \
+  --from-file=drivers.yaml=conf/aias/drivers.yaml  \
+  --from-file=aproc.yaml=conf/aias/aproc.yaml  \
+  --from-file=fam.yaml=conf/aias/fam.yaml  \
+  --dry-run=client  \
+  -o yaml > k8s/charts/fam/templates/fam-files-configmap.yaml
 
 helm dependency update k8s/charts/aias 
 helm dependency build k8s/charts/aias
