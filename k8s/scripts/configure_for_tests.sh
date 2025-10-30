f6f50453c1e68d25fc31c3479e6add7edae28c21
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2025-10-30 10:24:01 +0100

    fix cutom for tests

--- k8s/scripts/configure_for_tests.sh
+++ k8s/scripts/configure_for_tests.sh
@@ -3,11 +3,12 @@ set -o errexit -o pipefail
 IP=$(hostname -I | awk '{print $1}')
 DOMAIN=${IP}.nip.io
 
-yq eval \
-    '.global.dnsDomain = "'site.${DOMAIN}'"
-    | .global.elasticDnsDomain = "'elastic.${DOMAIN}'" 
-    | .global.keycloakDnsDomain = "'keycloak.${DOMAIN}'" 
-    | .global.keycloak.url = "'https://keycloak.${DOMAIN}/auth'" 
-    | .global.authIssuer = "'https://keycloak.${DOMAIN}/auth/realms/arlas'" 
-    | .global.openIdProvider = "'https://keycloak.${DOMAIN}/auth/realms/arlas/.well-known/openid-configuration'"' \
-    -i k8s/charts/arlas-stack/values.yaml
+
+echo "
+global:
+    dnsDomain: 'site.${DOMAIN}'
+    elasticDnsDomain: 'elastic.${DOMAIN}'
+    keycloakDnsDomain: 'keycloak.${DOMAIN}'
+    keycloak.url: 'https://keycloak.${DOMAIN}/auth'
+    authIssuer: 'https://keycloak.${DOMAIN}/auth/realms/arlas'
+    openIdProvider: 'https://keycloak.${DOMAIN}/auth/realms/arlas/.well-known/openid-configuration'" > custom_values.yaml
