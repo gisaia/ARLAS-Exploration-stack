f82b45a521467c891534f38478a98811740e3bb1
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-01-07 11:48:52 +0100

    show CI logs

--- scripts/show_logs.sh
+++ scripts/show_logs.sh
@@ -10,3 +10,18 @@ show apisix
 show keycloak
 show arlas-server
 show arlas-persistence-server
+
+if [ "$1" = "aias" ] || [ "$1" = "aiaskc" ]
+then
+    show airs-server
+    show aproc-service
+    show aproc-proc
+    show fam-service
+    show agate
+fi
+
+
+if [ "$1" = "iam" ] || [ "$1" = "aias" ]
+then
+    show arlas-iam-server
+fi
